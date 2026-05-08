ce6e226e9c2badadc59bebe19e424c3277446fbb
User <vargas.mattykun@gmail.com>
2026-05-08 22:08:13 +0800

    1.4.0

--- plugins/withNotifee.js
+++ plugins/withNotifee.js
@@ -1,21 +1,17 @@
-const { withSettingsGradle } = require("@expo/config-plugins");
+const { withProjectBuildGradle } = require("@expo/config-plugins");
 
 /**
- * Adds the notifee local Maven repo into the existing dependencyResolutionManagement
- * repositories block in settings.gradle so Gradle 7+ can resolve app.notifee:core,
- * which is bundled inside the package's android/libs.
+ * Adds the notifee local Maven repo into allprojects.repositories in build.gradle
+ * so Gradle can resolve app.notifee:core, bundled in the package's android/libs.
  */
 module.exports = function withNotifee(config) {
-  return withSettingsGradle(config, (mod) => {
+  return withProjectBuildGradle(config, (mod) => {
     const tag = "// notifee-maven";
     if (mod.modResults.contents.includes(tag)) return mod;
 
-    const mavenEntry = `        ${tag}\n        maven { url("$rootDir/../node_modules/@notifee/react-native/android/libs") }\n`;
-
-    // Insert before the closing brace of the repositories { } block
     mod.modResults.contents = mod.modResults.contents.replace(
-      /(dependencyResolutionManagement\s*\{[\s\S]*?repositories\s*\{)([\s\S]*?)(\})/,
-      (match, open, inner, close) => `${open}${inner}${mavenEntry}${close}`
+      /allprojects\s*\{\s*repositories\s*\{/,
+      `allprojects {\n  repositories {\n    ${tag}\n    maven { url("$rootDir/../node_modules/@notifee/react-native/android/libs") }`
     );
 
     return mod;
